1bf7e50923bcb12c16f54b91c01ba10ac3316a7b
kevwill1992 <kevinwilliams@fullsail.edu>
2023-12-06 23:44:53 -0800

    challenge 8

---
+++ python_collections.py
@@ -0,0 +1,22 @@
+# Script Name:                    Python Collection
+# Author:                         Kevin Williams
+# Date of Lastest revision:       12/6/2023
+# Purpose:                        Lists in Python
+# Additional Sources:            https://chat.openai.com/share/d98251ba-30aa-4fbc-a9ca-83ffedf0d56e
+
+# Declaration of Variables 
+my_list = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon"]
+
+
+print("Fourth element:", my_list[3])
+
+
+print("Sixth through tenth elements:", my_list[5:10])
+
+
+my_list[6] = "onion"
+
+
+print("Updated list:", my_list)
+
+# done
